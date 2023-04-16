099b762b9335c370d9afcb5ebf9c734ead24338a
Josoel Vieira <cvl.jv1@gmail.com>
2023-04-16 14:04:25 -0300

    capas dos disco

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import {
   LibraryIcon,
   ChevronLeft,
   ChevronRight,
+  PlayIcon,
 } from "lucide-react";
 import Image from "next/image";
 
@@ -77,69 +78,169 @@ export default function Home() {
           </div>
           <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
           <div className="grid grid-cols-3 gap-6">
-            <div className=" bg-white/10 overflow-hidden  rounded">
+            <a
+              href=""
+              className=" bg-white/10 group overflow-hidden  group rouded flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
               <Image
-                width={120}
-                height={120}
-                src="/src/Red_Hot_Chili_Peppers_-_Californication.jpg"
-                alt="Capa Album red hot"
-              />
-              <strong>Carlifornaction</strong>
-            </div>
-            <div className=" bg-white/10 overflow-hidden  rounded">
-              <Image
-                width={120}
-                height={120}
-                src="/src/Red_Hot_Chili_Peppers_-_Californication.jpg"
-                alt="Capa Album red hot"
-              />
-              <strong>Carlifornaction</strong>
-            </div>
-            <div className=" bg-white/10 overflow-hidden  rounded">
-              <Image
-                width={120}
-                height={120}
-                src="/src/Red_Hot_Chili_Peppers_-_Californication.jpg"
-                alt="Capa Album red hot"
-              />
-              <strong>Carlifornaction</strong>
-
-            </div>
-            <div className=" bg-white/10 overflow-hidden  rounded">
-              <Image
-                width={120}
-                height={120}
-                src="/src/Red_Hot_Chili_Peppers_-_Californication.jpg"
-                alt="Capa Album red hot"
-              />
-              <strong>Carlifornaction</strong>
-
-            </div>
-            <div className=" bg-white/10 overflow-hidden  rounded">
-              <Image
-                width={120}
-                height={120}
-                src="/src/Red_Hot_Chili_Peppers_-_Californication.jpg"
-                alt="Capa Album red hot"
-              />
-              <strong>Carlifornaction</strong>
-
-            </div>
-            <div className=" bg-white/10 overflow-hidden  rounded">
-              <Image
-                width={120}
-                height={120}
+                width={104}
+                height={104}
                 src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
                 alt="Capa Album red hot"
               />
               <strong>Carlifornaction</strong>
-
-            </div>
+              <button className="invisible p-3 pl-4 w-12 h-12 flex itens-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+            <a
+              href=""
+              className=" bg-white/5 group overflow-hidden  rouded flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+              />
+              <strong>Carlifornaction</strong>
+              <button className="invisible p-3 pl-4 w-12 h-12 flex itens-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+            <a
+              href=""
+              className=" bg-white/5 group overflow-hidden  rouded flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+              />
+              <strong>Carlifornaction</strong>
+              <button className="invisible p-3 pl-4 w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+            <a
+              href=""
+              className=" bg-white/5 group overflow-hidden  rouded flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+              />
+              <strong>Carlifornaction</strong>
+              <button className="invisible p-3 pl-4 w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+            <a
+              href=""
+              className=" bg-white/5 group overflow-hidden  rouded flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+              />
+              <strong>Carlifornaction</strong>
+              <button className="invisible p-3 pl-4 w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+            <a
+              href=""
+              className=" bg-white/5 group overflow-hidden  rouded flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+              />
+              <strong>Carlifornaction</strong>
+              <button className="invisible p-3 pl-4 w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+          </div>
+          <h2 className="font-semibold text-2xl mt-10">
+            Made for Josoel Vieira
+          </h2>
+          <div className="grid grid-cols-5 gap-4 mt-4">
+            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 ">
+            <Image
+                width={120}
+                height={120}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+                className="w-full"
+              />
+              <strong className="font-semibold">Californication</strong>
+              <span className="text-sm text-zinc-500">Wallows, Coin, girl in red</span>
+            </a>
+            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+            <Image
+                width={120}
+                height={120}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+                className="w-full"
+              />
+              <strong className="font-semibold">Californication</strong>
+              <span className="text-sm text-zinc-500">Wallows, Coin, girl in red</span>
+            </a>
+            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+            <Image
+                width={120}
+                height={120}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+                className="w-full"
+              />
+              <strong className="font-semibold">Californication</strong>
+              <span className="text-sm text-zinc-500">Wallows, Coin, girl in red</span>
+            </a>
+            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+            <Image
+                width={120}
+                height={120}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+                className="w-full"
+              />
+              <strong className="font-semibold">Californication</strong>
+              <span className="text-sm text-zinc-500">Wallows, Coin, girl in red</span>
+            </a>
+            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+            <Image
+                width={120}
+                height={120}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+                className="w-full"
+              />
+              <strong className="font-semibold">Californication</strong>
+              <span className="text-sm text-zinc-500">Wallows, Coin, girl in red</span>
+            </a>
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+        <div><Image
+                width={120}
+                height={120}
+                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+                alt="Capa Album red hot"
+                className="w-full"
+              /></div>
+        <div></div>
+        <div></div>
       </footer>
     </div>
   );
