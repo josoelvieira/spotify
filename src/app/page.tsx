85188a7b23ef00dfe88686b69992e3842944e54b
Josoel Vieira <cvl.jv1@gmail.com>
2023-04-16 18:54:41 -0300

    play de musica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Maximize2Icon } from "lucide-react";
 import {
   HomeIcon,
   SearchIcon,
@@ -5,6 +6,17 @@ import {
   ChevronLeft,
   ChevronRight,
   PlayIcon,
+  ShuffleIcon,
+  SkipBack,
+  PlayCircle,
+  SkipForward,
+  Repeat1Icon,
+  RepeatIcon,
+  Mic2,
+  LayoutList,
+  Laptop2Icon,
+  VolumeIcon,
+  MaximizeIcon,
 } from "lucide-react";
 import Image from "next/image";
 
@@ -173,8 +185,11 @@ export default function Home() {
             Made for Josoel Vieira
           </h2>
           <div className="grid grid-cols-5 gap-4 mt-4">
-            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 ">
-            <Image
+            <a
+              href=""
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 "
+            >
+              <Image
                 width={120}
                 height={120}
                 src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
@@ -182,10 +197,15 @@ export default function Home() {
                 className="w-full"
               />
               <strong className="font-semibold">Californication</strong>
-              <span className="text-sm text-zinc-500">Wallows, Coin, girl in red</span>
+              <span className="text-sm text-zinc-500">
+                Wallows, Coin, girl in red
+              </span>
             </a>
-            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-            <Image
+            <a
+              href=""
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
                 width={120}
                 height={120}
                 src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
@@ -193,10 +213,15 @@ export default function Home() {
                 className="w-full"
               />
               <strong className="font-semibold">Californication</strong>
-              <span className="text-sm text-zinc-500">Wallows, Coin, girl in red</span>
+              <span className="text-sm text-zinc-500">
+                Wallows, Coin, girl in red
+              </span>
             </a>
-            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-            <Image
+            <a
+              href=""
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
                 width={120}
                 height={120}
                 src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
@@ -204,10 +229,15 @@ export default function Home() {
                 className="w-full"
               />
               <strong className="font-semibold">Californication</strong>
-              <span className="text-sm text-zinc-500">Wallows, Coin, girl in red</span>
+              <span className="text-sm text-zinc-500">
+                Wallows, Coin, girl in red
+              </span>
             </a>
-            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-            <Image
+            <a
+              href=""
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
                 width={120}
                 height={120}
                 src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
@@ -215,10 +245,15 @@ export default function Home() {
                 className="w-full"
               />
               <strong className="font-semibold">Californication</strong>
-              <span className="text-sm text-zinc-500">Wallows, Coin, girl in red</span>
+              <span className="text-sm text-zinc-500">
+                Wallows, Coin, girl in red
+              </span>
             </a>
-            <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
-            <Image
+            <a
+              href=""
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
                 width={120}
                 height={120}
                 src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
@@ -226,21 +261,60 @@ export default function Home() {
                 className="w-full"
               />
               <strong className="font-semibold">Californication</strong>
-              <span className="text-sm text-zinc-500">Wallows, Coin, girl in red</span>
+              <span className="text-sm text-zinc-500">
+                Wallows, Coin, girl in red
+              </span>
             </a>
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
-        <div><Image
-                width={120}
-                height={120}
-                src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
-                alt="Capa Album red hot"
-                className="w-full"
-              /></div>
-        <div></div>
-        <div></div>
+      <footer className="bg-zinc-900 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image
+            width={56}
+            height={56}
+            src="/Red_Hot_Chili_Peppers_-_Californication.jpg"
+            alt="Capa Album red hot"
+            className="w-full"
+          />
+          <div className="flex flex-col">
+            <strong className="font-normal">Californication</strong>
+            <span className="text-xs text-zinc-400">Red hot chilli pepers</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-6">
+            <ShuffleIcon size={20} className="text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+            <button className="p-3 pl-4 w-10 h-10 flex items-center justify-center rounded-full bg-white text-black ml-auto">
+              <PlayIcon />
+            </button>
+            <SkipForward size={20} className="text-zinc-200" />
+            <RepeatIcon size={20} className="text-zinc-200" />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0.31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600 ">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+
+            <span className="text-sm text-zinc-500">2.20</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <Mic2 size={16} />
+          <LayoutList size={16} />
+          <Laptop2Icon size={16} />
+          <div className="flex items-center gap-2">
+            <VolumeIcon size={16} />
+            <div className="h-1 rounded-full w-24 bg-zinc-600 ">
+              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
+            </div>
+          </div>
+          <Maximize2Icon size={16} />
+        </div>
       </footer>
     </div>
   );
